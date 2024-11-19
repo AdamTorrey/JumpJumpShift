b21fdd01a9fa268a0733ec01708dd389b33447e4
AdamTorrey <21atorrey@gmail.com>
2024-11-19 15:35:36 -0500

    2D, Jump Mechanics, and advanced movement preparation
    I made the change to 2D Rigidbody and 2D colliders. I have added a proper isGrounded check, and the jump mechanics are working (this includes a jump cut mechanic). I am preparing to add in a double jump and a wall jump. Also, I need to add in a fall speed cap to avoid the player going absurdly fast while falling for long periods of time.
    Finally, I will start making a few levels and having playtests soon. Easier to fix mechanics now than later in the design process.

--- Jump Jump Shift/Assets/_Scripts/Jumper.cs
+++ Jump Jump Shift/Assets/_Scripts/Jumper.cs
@@ -5,24 +5,66 @@ using UnityEngine;
 
 public class Jumper : MonoBehaviour
 {
+    public Rigidbody2D RB;
+    public float gravityScale; // Normal Gravity
+
     [Header("Inscribed")]
     public float maxSpeed = 20; // The fastest I want the jumper to move
     public float acceleration = 2; // How fast the jumper gains speed
     public float deceleration = 1.75f; // How fast the jumper loses speed
+    public float jumpHeight = 15f; // The force with which the jumper jumps
+    public float gravityMult = 1.5f; // The multiplier for gravity when falling
+    public float jumpCutMult = .5f; // The multiplier for cutting a jump short
 
     [Header("Dynamic")]
     public float horizonSpeed = 0; // the jumpers current speed
+    public bool isGrounded;
+    public bool doubleJump;
+    public bool wallSliding;
+    public float sinceLastJump; //Time since last jump
+    
+    [Space]
 
-    // Start is called before the first frame update
+    //Used for jumping
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private Vector2 _groundCheckSize = new Vector2(.5f, .01f);
+
+    [Space]
+
+    [SerializeField] private Transform _frontWallCheck;
+    [SerializeField] private Transform _backWallCheck;
+    [SerializeField] private Vector2 _wallCheckSize = new Vector2(0.5f, 1f);
+
+    [SerializeField] private LayerMask _groundLayer;
+
+    void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-        
+        gravityScale = RB.gravityScale;
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapBox(_groundCheck.position, _groundCheckSize, 0, _groundLayer);
+        if ((Input.GetKeyDown(KeyCode.Space)) & isGrounded == true)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)){
+            JumpCut();
+        }
     }
 
     void FixedUpdate()
     {
         float xAxis = Input.GetAxisRaw("Horizontal");
         Vector3 pos = transform.position;
+
 
         horizonSpeed = horizonSpeed + (acceleration * xAxis);
 
@@ -43,6 +85,30 @@ public class Jumper : MonoBehaviour
 
         pos.x = pos.x + horizonSpeed * Time.deltaTime;
         transform.position = pos;
-        
+
+        if (RB.velocity.y < 0 & !wallSliding)
+        {
+            RB.gravityScale = RB.gravityScale * gravityMult;
+        }
+        else
+        {
+            RB.gravityScale = gravityScale;
+        }
+
+        sinceLastJump = sinceLastJump + Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        RB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        sinceLastJump = 0;
+    }
+
+    void JumpCut()
+    {
+        if (RB.velocity.y > 0 & sinceLastJump < 1.4)
+        {
+            RB.velocity = new Vector2(RB.velocity.x, RB.velocity.y * jumpCutMult);
+        }
     }
 }
